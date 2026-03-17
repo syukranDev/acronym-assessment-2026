f40b9c0702b31bda8c9dcb00082f089178621804
syukrandev <syukran.dev@gmail.com>
2026-03-17 11:53:14 +0800

    Add nuxtjs boilerplate

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
